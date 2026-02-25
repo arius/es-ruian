1050e75cf335f40d05c48fde921369e24dd8370d
arius <honza@arius.cz>
2026-02-25 19:14:29 +0100

    https base url

--- lib/es_ruian/configuration.rb
+++ lib/es_ruian/configuration.rb
@@ -27,7 +27,7 @@ module EsRuian
       end
 
       def set_defaults
-        @api_url = 'http://ruian.eurosignal.cz/api'
+        @api_url = 'https://ruian.eurosignal.cz/api'
       end
     end
   end
